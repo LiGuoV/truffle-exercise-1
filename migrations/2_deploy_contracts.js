177c4909f09e89e75fceafc7c3feef025a7169da
$ <ttmydlydl@gmail.com>
2022-07-31 22:04:17 +0800

    init commit

---
+++ migrations/2_deploy_contracts.js
@@ -0,0 +1,13 @@
+// const ConvertLib = artifacts.require("ConvertLib");
+// const MetaCoin = artifacts.require("MetaCoin");
+//
+// module.exports = function(deployer) {
+//   deployer.deploy(ConvertLib);
+//   deployer.link(ConvertLib, MetaCoin);
+//   deployer.deploy(MetaCoin);
+// };
+
+const StarNotary = artifacts.require('./StarNotary.sol')
+module.exports = function(deployer) {
+  deployer.deploy(StarNotary);
+};
---
+++ readme.md
@@ -0,0 +1,15 @@
+# 操作步骤
+``
+git init
+git remote add origin git@github.com:LiGuoV/truffle-exercise-1.git
+git branch -M main
+
+``
+- 
+- 
+- truffle develop 运行 Truffle 开发环境
+- compile编译智能合约中的任何更改。
+- migrate --reset迁移并创建一个全新且干净的智能合约会话。
+- test运行测试用例
+# 新窗口
+project/app > `npm run dev`
